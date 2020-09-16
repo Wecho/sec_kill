def058efdb74bf27658131c88268db167a3c7236
Wecho <wangyaooay@163.com>
2020-09-16 23:46:32 +0800

    Initial spring boot project

---
+++ src/main/java/com/wecho/top10_news_web/Top10NewsWebApplication.java
@@ -0,0 +1,13 @@
+package com.wecho.top10_news_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Top10NewsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Top10NewsWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wecho/top10_news_web/Top10NewsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wecho.top10_news_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Top10NewsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
